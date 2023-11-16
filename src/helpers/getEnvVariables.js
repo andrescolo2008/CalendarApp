0a2d6b9224bd32c0c57c83cc3392305ff19a3230
Andres Correa <andrescolo2008gmail.com>
2023-11-15 20:18:58 -0500

    .env.production

--- src/helpers/getEnvVariables.js
+++ src/helpers/getEnvVariables.js
@@ -2,10 +2,11 @@
 
  export const getEnvVaraibles= ( ) =>{
 
-    import.meta.env;
+    // import.meta.env;
 
     return{
-        ...import.meta.env
+        // ...import.meta.env
+        VITE_API_URL:import.meta.env.VITE_API_URL
     }
 
 }
